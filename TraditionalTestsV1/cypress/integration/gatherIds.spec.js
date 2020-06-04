7910e8309920b0626ab1e7d160287c07c363c64e
iGoog <paul@picayune.co>
2020-06-04 12:57:03 -0700

    broken tests, being a bit more clever with what to test. ran into this issue: https://github.com/cypress-io/cypress/issues/1534 will try https://github.com/andrewmcoupe/cy-view

--- TraditionalTestsV1/cypress/integration/gatherIds.spec.js
+++ TraditionalTestsV1/cypress/integration/gatherIds.spec.js
@@ -1,12 +1,101 @@
-import buildViews from '../fixtures/buildViews';
+const browser = `${Cypress.browser.name}_v${Cypress.browser.majorVersion}`;
+const views = [
+	{   size : [1200, 700],
+		showIds: [],
+		hideIds: [],
+		browser
+	}, {   size : [768, 700],
+		showIds: [],
+		hideIds: [],
+		browser
+	}, {   size : [500,700], // these are different... ['iphone-x', 'landscape'],
+		showIds: [],
+		hideIds: [],
+		browser
+	},
+];
+//
+// const buildViews = (url) => {
+// 	const docPromises = [];
+// 	const hiddenIdSet = new Set();
+// 	let remaining = views.length;
+// 	views.forEach((view) => {
+// 		cy.viewport(view.size[0], view.size[1]);
+// 		cy.visit(url);
+// 		docPromises.push(cy.document().then(() => {
+// 			view.hideIds = Cypress.$(':hidden').toArray()
+// 				.filter(el => el.id != '' && 'script' != el.tagName.toLowerCase())
+// 				.map( el => {
+// 					hiddenIdSet.add(el.id);
+// 					return el.id;
+// 				} );
+// 			if (--remaining==0) {
+// 				views.forEach((view) => {
+// 					view.showIds = new Set(hiddenIdSet);
+// 					view.hideIds.forEach(id => view.showIds.delete(id));
+// 					view.showIds = Array.from(view.showIds);
+// 				});
+//
+// 				console.log(JSON.stringify(views));
+// 			}
+// 		}));
+//
+// 	});
+// 	return Promise.allSettled(docPromises).then(results => {
+// 		return views;
+// 	});
+// };
 
-it('builds views with visible and invisible ids', () => {
-	const url = '/gridHackathonV1.html';
-	buildViews(url).then(views=> {
-		views.forEach(view => {
-			expect(view.showIds).greaterThan(0);
-		})
+const url = '/gridHackathonV1.html';
+
+let remaining = views.length;
+const hiddenIdSet = new Set();
+views.forEach((view) => {
+	const sizeText = `${view.size[0]},${view.size[1]}`;
+	describe(`Build all hide and display elements in ${browser} at ${sizeText}`, () => {
+		beforeEach(()=> {
+			cy.viewport(view.size[0], view.size[1]);
+			cy.visit(url);
+		});
+
+		it( `will have hidden elements in ${browser} at ${sizeText}`, () => {
+			cy.get('#page :visible > :hidden').then($hidden => {
+				view.hideIds = $hidden.toArray()
+					.filter(el => el.id != '' )
+					.map( el => {
+						hiddenIdSet.add(el.id);
+						return el.id;
+					} );
+				if (--remaining===0) {
+					views.forEach((v) => {
+						v.showIds = new Set(hiddenIdSet);
+						v.hideIds.forEach(id => v.showIds.delete(id));
+						v.showIds = Array.from(v.showIds);
+					});
+
+					console.log(JSON.stringify(views));
+				}
+				expect(view.hideIds.length).greaterThan(0);
+			});
+		});
+		it (`Hides ids in ${browser} at ${sizeText}`, () => {
+			for (let i=0; i < view.hideIds.length; i++) {
+				cy.get('#'+CSS.escape(view.hideIds[i])).should('be.hidden');
+			}
+			// view.hideIds.forEach((hideId) => {
+			// 	cy.get('#'+CSS.escape(hideId)).should('be.hidden');
+			// });
+		});
 	});
+});
 
-
-})
+// it('builds views with visible and invisible ids', () => {
+// 	const url = '/gridHackathonV1.html';
+// 	buildViews(url).then(views=> {
+// 		views.forEach(view => {
+// 			expect(view.showIds).greaterThan(0);
+// 		})
+// 	});
+//
+//
+// })
--- TraditionalTestsV1/cypress/integration/t1-crossDeviceElements.spec.js
+++ TraditionalTestsV1/cypress/integration/t1-crossDeviceElements.spec.js
@@ -1,37 +1,27 @@
 /// <reference types="cypress" />
 const views = require('../fixtures/views.json');
-
 const browser = `${Cypress.browser.name}_v${Cypress.browser.majorVersion}`;
+const url = '/gridHackathonV1.html';
 
-describe( 'Should display specific elements at given resolutions', () => {
-	const url = '/gridHackathonV1.html';
-	// buildViews(url);
-	views.forEach((view) => {
-		if (view.browser != browser) return;
-		const sizeText = `${view.size[0]},${view.size[1]}`;
-		it(`Shows elements in ${browser} at ${sizeText}`, () => {
+views.forEach((view) => {
+	if (view.browser != browser) return;
+	const sizeText = `${view.size[0]},${view.size[1]}`;
+	describe( `Display and hide appropriate elements in ${browser} at ${sizeText}`, () => {
+		before(()=> {
 			cy.viewport(view.size[0], view.size[1]);
 			cy.visit(url);
-			view.showIds.forEach((showId) => {
-				cy.get('#'+CSS.escape(showId)).should('be.visible');
-				// expect(.to.be.visible;
-			})
 		});
-		it(`Hides elements in ${browser} at ${sizeText}`, () => {
-			cy.viewport(view.size[0], view.size[1]);
-			cy.visit(url);
-			view.hideIds.forEach((hideId) => {
+		// view.showIds.forEach((showId) => {
+		// 	it (`Displays #${CSS.escape(showId)} in ${browser} at ${sizeText}`, () => {
+		// 		cy.get('#'+CSS.escape(showId)).should('be.visible');
+		// 	});
+		// });
+		view.hideIds.forEach((hideId) => {
+			it (`Hides #${CSS.escape(hideId)} in ${browser} at ${sizeText}`, () => {
 				cy.get('#'+CSS.escape(hideId)).should('be.hidden');
-			})
+			});
 		});
 
 	});
-
-
-
-
-
-
-
 
 });
